3c19321e3cf9b0dba5a906c3635086e87f835bf9
Ray021IDK <rr.lopez.2021@alumnos.urjc.es>
2026-09-18 23:44:27 +0200

    Add DELETE /user/{id} endpoint Refs #11

--- src/main/java/es/upm/miw/devops/controller/UserController.java
+++ src/main/java/es/upm/miw/devops/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
@@ -31,5 +32,10 @@ public class UserController {
             @RequestParam(required = false) String familyName,
             @RequestParam(required = false) Boolean billable) {
         return this.userService.findByFilter(name, familyName, billable);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id) {
+        this.userService.deleteById(id);
     }
 }
